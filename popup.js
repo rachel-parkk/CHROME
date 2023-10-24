70afb8cd9aec0fae8ee5c6980c1251e477ce3391
rachel-parkk <102939391+rachel-parkk@users.noreply.github.com>
2023-10-24 11:46:37 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,22 +1,42 @@
-// Get the input field and start button
 const timeInput = document.getElementById('time-input');
 const startButton = document.getElementById('start-button');
 
-// Event listener for the "start" button
 startButton.addEventListener('click', function () {
   const timeValue = timeInput.value;
   if (validateTimeInput(timeValue)) {
-    // You can use the timeValue for your timer functionality
-    // For now, let's display an alert with the entered time
-    alert(`Starting timer for ${timeValue}`);
+    // Disable the input field
+    timeInput.disabled = true;
+
+    // Parse the time input into hours, minutes, and seconds
+    const [hours, minutes, seconds] = timeValue.split(':').map(Number);
+    let totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
+    // Function to update the countdown
+    function updateCountdown() {
+      const hoursRemaining = Math.floor(totalSeconds / 3600);
+      const minutesRemaining = Math.floor((totalSeconds % 3600) / 60);
+      const secondsRemaining = totalSeconds % 60;
+
+      // Display the countdown in the input field
+      timeInput.value = `${String(hoursRemaining).padStart(2, '0')}:${String(minutesRemaining).padStart(2, '0')}:${String(secondsRemaining).padStart(2, '0')}`;
+
+      // Check if the countdown has reached zero
+      if (totalSeconds <= 0) {
+        clearInterval(countdownInterval);
+        alert('Countdown has finished!');
+      } else {
+        totalSeconds--;
+      }
+    }
+
+    // Update the countdown every second
+    const countdownInterval = setInterval(updateCountdown, 1000);
   } else {
     alert('Invalid time format. Please use hh:mm:ss format.');
   }
 });
 
-// Function to validate time input
 function validateTimeInput(input) {
-  // Simple regex to check if the input matches the format hh:mm:ss
   const timePattern = /^(\d{2}):(\d{2}):(\d{2})$/;
   return timePattern.test(input);
 }
